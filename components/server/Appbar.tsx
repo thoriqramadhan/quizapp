574459edbfa9ae2c7b115d07b664e607c48ca6c5
thoriqramadhan <respect.thoriq@gmail.com>
2024-12-19 14:01:05 +0700

    feat: add dynamic settings on appbar

--- components/server/Appbar.tsx
+++ components/server/Appbar.tsx
@@ -1,19 +1,46 @@
-import { LogIn, LogOut } from 'lucide-react';
-import { FC } from 'react';
+import { Backpack, icons, LogIn, LogOut, Pencil, Telescope } from 'lucide-react';
+import Link from 'next/link';
+import React, { FC } from 'react';
 
 interface AppbarProps {
 
 }
-
+type appBarItemType = {
+    title: string,
+    href: string,
+    icon?: React.ReactNode
+}
 const Appbar: FC<AppbarProps> = ({ }) => {
+    const iconSize = 17;
+    const appBarItem = [
+        {
+            title: 'Create',
+            href: "/home/create",
+            icon: <Pencil size={iconSize} />
+        },
+        {
+            title: 'Owned',
+            href: '/home/owned',
+            icon: <Backpack size={iconSize} />
+        },
+        {
+            title: 'Explore',
+            href: '/home/explore',
+            icon: <Telescope size={iconSize} />
+        }
+    ] as appBarItemType[]
     return <>
-        <nav className='hidden md:flex w-[170px] h-full flex-col bg-[#5E40D2] text-white px-5 py-5'>
-            <h1 className='font-bold text-xl'>Quizapp</h1>
+        <nav className='hidden md:flex w-[170px] h-full flex-col bg-[#5E40D2] text-white py-5'>
+            <h1 className='font-bold text-xl mx-5'>Quizapp</h1>
             <div className="mt-10">
                 <section className="w-full flex flex-col gap-y-3">
-                    <div className="w-full hover:bg-[#9984EA]/30 hover:backdrop-blur-md transition-300 p-1">
-                        <p>Test</p>
-                    </div>
+                    {
+                        appBarItem.map((item, index) => (
+                            <Link key={index} href={item.href} className="w-full hover:bg-[#9984EA]/30 hover:backdrop-blur-md transition-300 p-1 cursor-pointer flex gap-x-2 items-center px-5">
+                                {item.icon} {item.title}
+                            </Link>
+                        ))
+                    }
                 </section>
             </div>
         </nav>;
