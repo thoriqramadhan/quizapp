dbf125e48350a7ea874bcbd9bc258caa11733425
thoriqramadhan <respect.thoriq@gmail.com>
2024-12-31 11:24:13 +0700

    feat: update quiz context type

--- lib/context/createQuiz.tsx
+++ lib/context/createQuiz.tsx
@@ -1,17 +1,40 @@
 'use client'
 import { createContext, useContext, useState } from "react";
 
-const QuizContext = createContext<any>(undefined)
+type changeQuestionOptionProps = {
+    isInitial: boolean,
+    value: any
+}
+type QuestionObject = {
+    projectName: string,
+    coverImg: string,
+    tags: string,
+    quiz: any[]
+}
+type QuizContextType = {
+    question: QuestionObject,
+    handleChangeQuestion: (option: changeQuestionOptionProps) => void
+}
+
+const QuizContext = createContext<QuizContextType | undefined>(undefined)
 export function useQuiz() {
     const context = useContext(QuizContext)
+    if (!context) {
+        throw new Error("useQuiz must be used within a QuizProvider");
+    }
     return context;
 }
 
 export function QuizProvider({ children }) {
-    const [question, setQuestion] = useState({})
-    function handleChangeQuestion() {
-        console.log('you using the context!');
-
+    const initialQuestionObject = {
+        projectName: '',
+        coverImg: '',
+        tags: '',
+        quiz: []
+    }
+    const [question, setQuestion] = useState<QuestionObject>(initialQuestionObject)
+    function handleChangeQuestion(option: changeQuestionOptionProps) {
+        console.log('you using the context!', option);
     }
     return <QuizContext.Provider value={{ question, handleChangeQuestion }}>
         {children}
