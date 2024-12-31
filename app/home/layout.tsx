e9197844f87612809eae81a16fa363e95e208201
thoriqramadhan <respect.thoriq@gmail.com>
2024-12-31 09:54:51 +0700

    fix: error overflow

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 const Layout: FC<LayoutProps> = ({ children }) => {
     return <main className='w-full h-screen flex'>
         <Appbar />
-        <section className='px-5 py-5 flex-1 md:pl-[180px]'>
+        <section className='py-5 px-5 md:pl-[180px] max-w-full overflow-x-hidden'>
             {children}
         </section>
     </main>
