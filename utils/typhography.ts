39b1beca4fb3d49fc3530acfc2108793592e867e
thoriqramadhan <respect.thoriq@gmail.com>
2025-01-03 13:31:52 +0700

    feat: add new utils

--- utils/typhography.ts
+++ utils/typhography.ts
@@ -4,3 +4,23 @@ export function capitalizeFirstLetter(value: string) {
     // value.charAt(0).toUpperCase() + value.slice(1);
     return textBuilder
 }
+
+export function getChoiceAplhabet(index: 0 | 1 | 2 | 3) {
+    if (index == 0) {
+        return 'a.'
+    } else if (index == 1) {
+        return 'b.'
+    } else if (index == 2) {
+        return 'c.'
+    } else {
+        return 'd.'
+    }
+}
+
+export function getChoiceWithoutAlphabet(value: string) {
+    if (!value) {
+        return ''
+    }
+    const splitedValue = value.split('.')
+    return splitedValue[1] 
+}
