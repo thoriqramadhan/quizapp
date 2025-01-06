4660effe19a7ce68aa54664ec985203dbaa5c738
thoriqramadhan <respect.thoriq@gmail.com>
2025-01-06 14:17:41 +0700

    feat: create DropdownItemContainer copmponents & add props for children in DropdownItem before rendering

--- components/client/Dropdown.tsx
+++ components/client/Dropdown.tsx
@@ -1,8 +1,8 @@
 'use client'
 import { cn } from '@/utils/style';
 import clsx from 'clsx';
-import { ChevronDown } from 'lucide-react';
-import { Dispatch, FC, SetStateAction, useEffect, useState } from 'react';
+import { ChevronDown, CircleEllipsis } from 'lucide-react';
+import React, { cloneElement, Dispatch, FC, HTMLAttributes, SetStateAction, useEffect, useState } from 'react';
 import { number } from 'zod';
 
 type OptionAlign = 'left' | 'center' | 'right'
@@ -14,7 +14,7 @@ interface DropdownProps {
     externalSetter?: (value: any) => void
 }
 
-const Dropdown: FC<DropdownProps> = ({ defaultValue, className, optionAlign, dropdownOptions, externalSetter }) => {
+export const Dropdown: FC<DropdownProps> = ({ defaultValue, className, optionAlign, dropdownOptions, externalSetter }) => {
     const [isOpen, setIsOpen] = useState(true);
     const [dropdownState, setDropdownState] = useState(defaultValue ?? 'Your data')
     useEffect(() => {
@@ -28,32 +28,86 @@ const Dropdown: FC<DropdownProps> = ({ defaultValue, className, optionAlign, dro
             <span className='relative h-fit'>
                 <div className={cn(`px-3 py-1 flex-all-center border gap-x-2 cursor-pointer`, className)} onClick={() => setIsOpen(prev => !prev)}>{dropdownState.toString()} <ChevronDown className={`transition-300 ${isOpen && 'rotate-180'}`} size={15} /></div>
                 {/* option V */}
-                <section className={cn(`z-50 absolute top-[calc(100%+5px)] rounded-md max-h-[300px] min-w-[100px] overflow-y-auto max-w-[185px] bg-red-500 scrollbar-thin shadow-md border transition-500 ${!isOpen && 'overflow-y-hidden h-[0px] border-none'}`)}>
+                <DropdownItemContainer isOpen={isOpen} >
                     {dropdownOptions.map((option, index) => (
                         <DropdownItem key={index} value={option} setter={setDropdownState} />
                     ))}
-                </section>
+                </DropdownItemContainer>
             </span >
         </>
     )
 }
 
 
-interface DropdownItemProps {
-    optionAlign?: OptionAlign
-    value: string | number,
-    setter: Dispatch<SetStateAction<string | number>>
+interface DropdownItemContainerProps {
+    children: React.ReactNode,
+    isOpen: boolean,
+    optionContainerAlign?: OptionAlign,
 }
 
-const DropdownItem: FC<DropdownItemProps> = ({ optionAlign, value, setter }) => {
-    return <div className={clsx('w-full py-1 px-2 bg-white transition-300 cursor-pointer hover:bg-zinc-50', {
+export const DropdownItemContainer: FC<DropdownItemContainerProps> = ({ children, isOpen, optionContainerAlign }) => {
+    return <section className={cn(`z-50 absolute top-[calc(100%+5px)] rounded-md max-h-[300px] min-w-[100px] overflow-x-hidden overflow-y-auto max-w-[185px] bg-red-500 scrollbar-thin shadow-md border transition-500 ${!isOpen && 'overflow-y-hidden h-[0px] border-none'}`, { '-translate-x-[calc(87%)]': optionContainerAlign == 'left' })}>
+        {children}
+    </section>
+}
+
+interface DropdownItemProps extends HTMLAttributes<HTMLDivElement> {
+    optionAlign?: OptionAlign
+    value: string | number,
+    className?: string,
+    callbackFn?: () => void | undefined,
+    setter?: Dispatch<SetStateAction<string | number>>
+}
+
+export const DropdownItem: FC<DropdownItemProps> = ({ optionAlign, value, className, setter, callbackFn, ...props }) => {
+    const getOnClickHandler = () => {
+        if (callbackFn) {
+            callbackFn()
+            console.log('called callback');
+
+        } else {
+            setter!(value)
+            console.log('called setter');
+
+        }
+    }
+    return <div className={cn('w-full py-1 px-2 max-h-[32px] whitespace-nowrap text-truncate overflow-y-auto bg-white transition-300 cursor-pointer hover:bg-zinc-50', {
         '': optionAlign == 'left',
         'flex-all-center': optionAlign == 'center',
         'flex justify-end items-center': optionAlign == 'right',
-    })}
-        onClick={() => setter(value)}>
+    }, className)} {...props}
+        onClick={getOnClickHandler}
+    >
         {value.toString()}
     </div>
 }
 
-export default Dropdown
+interface OptionDropdownProps {
+    optionAlign?: OptionAlign,
+    dropdownOptions?: (string | number)[],
+    children: React.ReactNode
+}
+
+export const OptionDropdown: FC<OptionDropdownProps> = ({ optionAlign, dropdownOptions, children }) => {
+    const [isOpen, setisOpen] = useState(false)
+    // cloneElement untuk memodifikasi sebuah component sebelum ke render.
+    const enhancedChildren = React.Children.map(children, (child) => {
+        if (React.isValidElement(child)) {
+            return cloneElement(child, { optionAlign })
+        }
+        return child;
+    })
+    return (
+        <>
+            <div className="relative">
+                <CircleEllipsis className='cursor-pointer' onClick={() => setisOpen(prev => !prev)} />
+                <DropdownItemContainer isOpen={isOpen} optionContainerAlign='left'>
+                    <DropdownItem optionAlign={optionAlign} value={'Hello'} />
+                    {enhancedChildren}
+                </DropdownItemContainer>
+            </div>
+        </>
+    )
+}
+
+
