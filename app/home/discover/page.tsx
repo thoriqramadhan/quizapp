e59b2ceecc4064a03eb6f5672c8c432e656066df
thoriqramadhan <respect.thoriq@gmail.com>
2025-01-08 15:17:09 +0700

    feat: connecting from explore to preview page

--- app/home/discover/page.tsx
+++ app/home/discover/page.tsx
@@ -9,7 +9,7 @@ interface DiscoverProps {
 const DiscoverPage: FC<DiscoverPageProps> = async ({ }) => {
     const quizData = (await getAllQuiz({ with: ['question', 'user'] })) as unknown as QuizObject[]
     return <>
-        <div className='text-title'>Explore Quiz</div>;
+        <div className='text-title'>Explore Quiz</div>
         <section className="w-full flex gap-5 flex-wrap">
             {
                 quizData.map((quiz, index) => (
