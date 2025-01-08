eaa76a133167a3d702471dace9448828504bb58f
thoriqramadhan <respect.thoriq@gmail.com>
2025-01-08 15:16:48 +0700

    feat: add preview ui

---
+++ components/client/Container.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+interface DropdownOptionContainerProps {
+
+}
+
+const DropdownOptionContainer: FC<DropdownOptionContainerProps> = ({ }) => {
+    return <div>DropdownOptionContainer</div>;
+}
+
+export default DropdownOptionContainer;
