5e8ba5dfcdf3a3d701ecadc10742cc24cb12bb3f
thoriqramadhan <respect.thoriq@gmail.com>
2025-01-15 09:14:29 +0700

    feat: add result of quiz

--- app/play/[quizId]/_playPageComponents/_playPageContainer.tsx
+++ app/play/[quizId]/_playPageComponents/_playPageContainer.tsx
@@ -13,11 +13,18 @@ interface _PlayPageContainerProps {
 
 const _PlayPageContainer: FC<_PlayPageContainerProps> = ({ questions, quizId }) => {
     const [currentPage, setCurrentPage] = useState(1)
-    const quizPercentage = Math.floor((currentPage / questions.length) * 100);
+    const donePercentage = Math.floor((currentPage / questions.length) * 100);
     const currentQuestionInit = questions[currentPage - 1]
+
     const [currentQuestion, setCurrentQuestion] = useState<QuestionObjectDB>(currentQuestionInit)
     const [selectedChoice, setSelectedChoice] = useState('e.');
     const [playerOngoingAnswer, setPlayerOngoingAnswer] = useState(JSON.parse(localStorage.getItem(`playerQuizData-${quizId}`)) || [])
+
+    const [quizResultData, setQuizResultData] = useState({
+        quizPercentage: '',
+        stats: []
+    });
+
 
     function handleUpdate() {
         let isChoiceValid = false;
@@ -37,11 +44,36 @@ const _PlayPageContainer: FC<_PlayPageContainerProps> = ({ questions, quizId }) 
         return false
 
     }
+    function handleSubmit() {
+        let correctAnswer = 0;
+        const userStatistic = {
+            quizPercentage: '',
+            totalCorrectChoice: 0,
+            stats: []
+        };
+        const userAnswer = JSON.parse(localStorage.getItem(`playerQuizData-${quizId}`))
+
+        for (let index = 0; index < questions.length; index++) {
+            let status = 'salah'
+            if (userAnswer[index].selectedChoice == questions[index].correctChoice) {
+                userStatistic.totalCorrectChoice += 1
+                status = 'benar'
+            }
+            const finalData = {
+                quizNo: index + 1,
+                selectedChoice: userAnswer[index].selectedChoice,
+                correctChoice: questions[index].correctChoice,
+                status
+            }
+            userStatistic.stats.push(finalData)
+        }
+        const quizPercentagePoint = `${Math.floor(((userStatistic.totalCorrectChoice / questions.length) * 100))}%`
+        userStatistic.quizPercentage = quizPercentagePoint
+        setQuizResultData(userStatistic)
+
+    }
     function handleNextQuestion(option: 'prev' | 'next') {
         if (option == 'next') {
-            if (currentPage == questions.length) {
-                return
-            }
             const isValid = handleUpdate()
             if (isValid) {
                 setCurrentPage(prev => prev + 1)
@@ -54,51 +86,55 @@ const _PlayPageContainer: FC<_PlayPageContainerProps> = ({ questions, quizId }) 
             setCurrentPage(prev => prev - 1)
         }
     }
-    console.log(selectedChoice);
-
     useEffect(() => {
         setCurrentQuestion(currentQuestionInit)
         const selectedInit = playerOngoingAnswer[currentPage - 1]?.selectedChoice ?? 'da'
-        console.log(selectedInit);
-
         setSelectedChoice(selectedInit)
+        if (currentPage > questions.length) {
+            handleSubmit()
+            return
+        }
     }, [currentPage])
     return (
         <>
-            <section className="w-full flex justify-between items-center">
-                <p className='font-semibold '>{currentPage}/{questions.length}</p>
-                <p className='text-xl font-semibold'>Quiz</p>
-                <OptionDropdown >
-                    <DropdownItem value={'test'} />
-                </OptionDropdown>
-            </section>
-            {/* content */}
-            <section className='space-y-5 pt-5'>
-                {/* progress bar */}
-                <div className="w-full rounded-full h-[20px] bg-zinc-100 overflow-hidden" id='progress-bar'>
-                    <div className=" h-full  rounded-r-full bg-red-400 flex items-center justify-end pr-2" style={{ width: `${quizPercentage}%` }}>
-                        <p className='text-xs text-white'>{quizPercentage}%</p>
-                    </div>
-                </div>
+            {currentQuestion ?
+                <>
+                    <section className="w-full flex justify-between items-center">
+                        <p className='font-semibold '>{currentPage}/{questions.length}</p>
+                        <p className='text-xl font-semibold'>Quiz</p>
+                        <OptionDropdown >
+                            <DropdownItem value={'test'} />
+                        </OptionDropdown>
+                    </section>
+                    <section className='space-y-5 pt-5 my-5'>
+                        {/* progress bar */}
+                        <div className="w-full rounded-full h-[20px] bg-zinc-100 overflow-hidden" id='progress-bar'>
+                            <div className=" h-full  rounded-r-full bg-red-400 flex items-center justify-end pr-2" style={{ width: `${donePercentage}%` }}>
+                                <p className='text-xs text-white'>{donePercentage}%</p>
+                            </div>
+                        </div>
 
-                {/* img */}
-                <div className="w-full h-[250px] bg-zinc-50 border rounded-xl"></div>
-                <div className="text-center px-[70px]">
-                    <p className='font-semibold text-lg tex-slate-700'>{currentQuestion.question}</p>
-                </div>
-                <hr />
-                <section className='grid grid-cols-2 w-full h-[400px] gap-5'>
-                    {
-                        cardColor.map((item, index) => (
-                            <PlayPageCard key={index} choiceText={currentQuestion.choice[index]} index={index} setSelectedChoice={setSelectedChoice} mainColor={item.mainColor} shadowColor={item.shadowColor} isSelected={getChoiceAplhabet(index) == selectedChoice} />
-                        ))
-                    }
-                </section>
-                <div className="w-full flex items-end justify-between">
-                    <Button className='' onClick={() => handleNextQuestion('prev')}>Previous</Button>
-                    <Button className='' onClick={() => handleNextQuestion('next')}>{currentPage == questions.length ? 'Done' : 'Next'}</Button>
-                </div>
-            </section>
+                        {/* img */}
+                        <div className="w-full h-[250px] bg-zinc-50 border rounded-xl"></div>
+                        <div className="text-center px-[70px]">
+                            <p className='font-semibold text-lg tex-slate-700'>{currentQuestion.question}</p>
+                        </div>
+                        <hr />
+                        <section className='grid grid-cols-2 w-full h-[400px] gap-5'>
+                            {
+                                cardColor.map((item, index) => (
+                                    <PlayPageCard key={index} choiceText={currentQuestion.choice[index]} index={index} setSelectedChoice={setSelectedChoice} mainColor={item.mainColor} shadowColor={item.shadowColor} isSelected={getChoiceAplhabet(index) == selectedChoice} />
+                                ))
+                            }
+                        </section>
+                        <div className="w-full flex items-end justify-between">
+                            <Button className='' onClick={() => handleNextQuestion('prev')}>Previous</Button>
+                            <Button className='' onClick={() => handleNextQuestion('next')}>{currentPage == questions.length ? 'Done' : 'Next'}</Button>
+                        </div>
+                    </section>
+                </> :
+                <QuizResultStatistic quizResultData={quizResultData} />
+            }
         </>
     )
 }
@@ -126,6 +162,49 @@ const PlayPageCard: FC<playPageCardProps> = ({ mainColor, shadowColor, choiceTex
             {getChoiceWithoutAlphabet(choiceText, 1)}
         </label>
     )
+}
+
+
+interface QuizResultStatisticProps {
+    quizResultData: {
+        quizPercentage: string,
+        stats: never[],
+        totalCorrectChoice: number
+    }
+}
+
+const QuizResultStatistic: FC<QuizResultStatisticProps> = ({ quizResultData }) => {
+    return <div className='w-full h-screen flex-all-center '>
+        <div className="w-full min-h-[600px] border rounded-lg shadow-md bg-slate-700 py-10 px-10">
+            <section className="w-full flex justify-center flex-col items-center space-y-5 overflow-hidden">
+                <div className='text-5xl font-bold text-white'><span className='text-green-400'>{quizResultData.totalCorrectChoice}</span> / {quizResultData.stats.length}</div>
+                <div className="w-full flex gap-x-5">
+                    <p className='text-white font-bold'>{quizResultData.quizPercentage}</p>
+                    <div className='flex-1 h-5 rounded-md bg-red-400 overflow-hidden'>
+                        <span className='h-full bg-green-300 block ' style={{ width: quizResultData.quizPercentage }} ></span>
+                    </div>
+                </div>
+            </section>
+            <table className='bg-white table-auto w-full border-collapse border border-slate-500 text-center mt-5'>
+                <thead>
+                    <tr>
+                        <th className='border border-slate-600'>No</th>
+                        <th className='border border-slate-600'>Selected Choice</th>
+                        <th className='border border-slate-600'>Correct Choice</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {quizResultData.stats.map((item, index) => (
+                        <tr className={`${item.status == 'benar' ? 'bg-green-400' : 'bg-red-400'} text-white`} key={index}>
+                            <td className='border border-slate-600'>{item.quizNo}</td>
+                            <td className='border border-slate-600'>{item.selectedChoice}</td>
+                            <td className='border border-slate-600'>{item.correctChoice}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    </div>
 }
 
 export default _PlayPageContainer;
